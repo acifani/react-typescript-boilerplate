260d0f2b518301035344a286d415c0e9f3a7cb6d
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-08-26 18:40:55 +0200

    Split webpack config and add dev server

--- webpack.common.js
+++ webpack.common.js
@@ -3,8 +3,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
